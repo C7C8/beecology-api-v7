c092f7d16932c2fdc25455e6e1b6556788dd3a28
Sourec <tothemyers@gmail.com>
2019-12-19 18:39:41 -0500

    Implement video uploading

--- api_services/upload_media.py
+++ api_services/upload_media.py
@@ -15,9 +15,14 @@ log = getLogger()
 
 class UploadVideo(Resource):
 	@staticmethod
-	def post():
+	@authenticate
+	def post(user):
 		"""Upload a video"""
-		return "Placeholder"
+		parser = reqparse.RequestParser()
+		parser.add_argument("recordVideo", type=str, required=True)
+		args = parser.parse_args()
+
+		return process_media(args["recordVideo"], "video", user)
 
 
 class UploadImage(Resource):
@@ -29,27 +34,31 @@ class UploadImage(Resource):
 		parser.add_argument("recordImage", type=str, required=True)
 		args = parser.parse_args()
 
-		# Decode file from base64, then make sure it's actually an image
-		mime = magic.Magic(mime=True)
-		data = base64.standard_b64decode(args["recordImage"])
-		mime_type = mime.from_buffer(data)
-		if mime_type.find("image/") == -1:
-			log.warning("User {} attempted to upload file with wrong MIME type, {}".format(user, mime_type))
-			return response("false", "Received file but was of type {} instead of image/*".format(mime_type), True)
+		return process_media(args["recordImage"], "image", user)
 
-		# Save image file to UUID-based name, with an extension determined by the file's MIME type
-		# Using a UUID allows us to guarantee unique filenames while embedding user and time information
-		# The node computation takes the SHA-1 of the username (similar to the original Node server) and
-		# uses it as the node value mod 2^48 (node values are 48 bits). This lets us embed user info in
-		# the filename, but not in a back-traceable way.
-		node = None if user is None else int(hashlib.sha1(user.encode("utf-8")).hexdigest(), 16) % (1 << 48)
-		filename = "{dir}/{uuid}.{ext}".format(dir=Config.config["storage"]["imageUploadPath"],
-		                                       uuid=uuid.uuid1(node=node),
-		                                       ext=mime_type.split("/")[1])
-		log.info("Saving image to {}".format(filename))
-		with open(filename, "wb") as file:
-			file.write(data)
 
-		res = response("success", "upload image success", False)
-		res["imagePath"] = filename
-		return res, 200
+def process_media(b64, mime_type, user):
+	"""Process a media file from base64, validating its mime type in the process."""
+	data = base64.standard_b64decode(b64)
+	mime = magic.Magic(mime=True)
+	file_type = mime.from_buffer(data)
+	if file_type.find(mime_type) == -1:
+		log.warning("User {} attempted to upload file with wrong MIME type, {}".format(user, file_type))
+		return response("false", "Received file but was of type {} instead of {}/*".format(file_type, mime_type), True)
+
+	# Save image file to UUID-based name, with an extension determined by the file's MIME type
+	# Using a UUID allows us to guarantee unique filenames while embedding user and time information
+	# The node computation takes the SHA-1 of the username (similar to the original Node server) and
+	# uses it as the node value mod 2^48 (node values are 48 bits). This lets us embed user info in
+	# the filename, but not in a back-traceable way.
+	node = None if user is None else int(hashlib.sha1(user.encode("utf-8")).hexdigest(), 16) % (1 << 48)
+	filename = "{dir}/{uuid}.{ext}".format(dir=Config.config["storage"]["imageUploadPath"],
+	                                       uuid=uuid.uuid1(node=node),
+	                                       ext=file_type.split("/")[1])
+	log.info("Saving image to {}".format(filename))
+	with open(filename, "wb") as file:
+		file.write(data)
+
+	res = response("success", "upload image success", False)
+	res["imagePath"] = filename
+	return res, 200
