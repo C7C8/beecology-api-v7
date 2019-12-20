bc7188fc5de21212ee934e331fa1e36478c5726c
Sourec <tothemyers@gmail.com>
2019-12-20 13:47:01 -0500

    Implement unenroll endpoint, improve authentication

--- api_services/authentication.py
+++ api_services/authentication.py
@@ -3,7 +3,9 @@ from logging import getLogger
 
 from firebase_admin import auth
 from flask import request
+from sqlalchemy import sql, and_
 
+from api_services.database import Database
 from api_services.utility import response
 
 log = getLogger()
@@ -12,14 +14,21 @@ def authenticate(func):
 	"""Decorator for user authentication"""
 	def wrapper(*args, **kwargs):
 		if "Authorization" not in request.headers:
-			return response("false", "Authorization required", True), 401
-		decoded = base64.standard_b64decode(request.headers["Authorization"].split(" ")[1]).decode("utf-8")
-		tokens = decoded.split(":")
+			return response("false", "Authorization required", True), 403
 		try:
-			uid = auth.verify_id_token(tokens[1])["uid"]
+			decoded = base64.standard_b64decode(request.headers["Authorization"].split(" ")[1]).decode("utf-8")
+			firebase_token, access_token = decoded.split(":")
+			uid = auth.verify_id_token(access_token)["uid"]
 		except Exception as e:
-			log.info("User ID {} failed authentication: ".format(tokens[0], e))
+			log.info("User failed authentication: {}".format(e))
 			return response("false", "Failed to authenticate: {}".format(e), True), 403
+
+		with Database() as engine:
+			query = sql.select([Database.auth]).where(and_(Database.auth.c.user_id == uid,
+			                                               Database.auth.c.access_token == access_token))
+			results = engine.execute(query)
+			if len(next(results, {})) == 0:
+				return response("false", "Authorization failed, please enroll first", True), 403
 
 		log.info("Authenticated user \"{}\"".format(uid))
 		return func(*args, **kwargs, user=uid)
--- api_services/user.py
+++ api_services/user.py
@@ -6,7 +6,7 @@ from logging import getLogger
 from firebase_admin import auth
 from flask import request
 from flask_restplus import Resource, reqparse
-from sqlalchemy import sql
+from sqlalchemy import sql, and_
 
 from .config import Config
 from api_services.authentication import authenticate
@@ -14,6 +14,7 @@ from api_services.database import Database
 from api_services.utility import response
 
 log = getLogger()
+
 
 class RecordData(Resource):
 	@staticmethod
@@ -71,17 +72,17 @@ class Enroll(Resource):
 		"""Enroll; uses a firebase token via basic auth to get a generate an access token"""
 		if "Authorization" not in request.headers or request.headers["Authorization"].find("Basic") == -1:
 			return response("false", "Firebase authorization token required", True), 403
-		token = base64.standard_b64decode(request.headers["authorization"].split(" ")[1])
 		try:
+			token = base64.standard_b64decode(request.headers["Authorization"].split(" ")[1])
 			uid = auth.verify_id_token(token)["uid"]
 		except Exception as e:
-			log.error("Firebase token failed validation: {}".format(e))
+			log.warning("Firebase token failed validation: {}".format(e))
 			return response("false", "Firebase token failed validation", True), 403
 
 		# Generate token (1024 random bytes), expiration,
 		accessToken = secrets.token_hex(1024)
 		refreshToken = secrets.token_hex(1024)
-		expiration = datetime.now() + timedelta(seconds=Config.config["auth"]["token-expiration"])
+		expiration = datetime.now() + timedelta(seconds=Config.config["auth"]["token-lifetime"])
 
 		# Delete all other entries for this user and insert a new auth table entry. This effectively logs the user out
 		# of all other devices, and helps keep the auth table clean.
@@ -94,12 +95,13 @@ class Enroll(Resource):
 		log.info("Enrolled user {}".format(uid))
 
 		return {
-			"accessToken": accessToken,
-			"refreshToken": refreshToken,
-			"expiresIn": Config.config["auth"]["token-lifetime"] * 1000,
-			"expiresAt": int(expiration.timestamp() * 1000),
-			"type": "Bearer"
-		}, 200
+			       "accessToken": accessToken,
+			       "refreshToken": refreshToken,
+			       "expiresIn": Config.config["auth"]["token-lifetime"] * 1000,
+			       "expiresAt": int(expiration.timestamp() * 1000),
+			       "type": "Bearer"
+		       }, 200
+
 
 class Refresh(Resource):
 	@staticmethod
@@ -107,17 +109,18 @@ class Refresh(Resource):
 		"""Refresh a token"""
 		if "Authorization" not in request.headers or request.headers["Authorization"].find("Bearer") == -1:
 			return response("false", "Authorization required", True), 403
-		refresh_token, firebase_token = base64.standard_b64decode(request.headers["authorization"].split(" ")).split(":")
 		try:
+			decoded = base64.standard_b64decode(request.headers["Authorization"].split(" ")[1])
+			refresh_token, firebase_token = decoded.decode("utf-8").split(":")
 			uid = auth.verify_id_token(firebase_token)["uid"]
 		except Exception as e:
-			log.error("Firebase token failed validation".format(e))
+			log.warning("Firebase token failed validation".format(e))
 			return response("false", "Firebase token failed validation", True), 403
 
 		# Verify we have an entry in the auth table corresponding to this uid+token combo
 		with Database() as engine:
-			results = engine.execute(sql.select(Database.auth).where(Database.auth.c.user_id == uid
-			                                                         and Database.auth.c.refresh_token == refresh_token))
+			results = engine.execute(sql.select([Database.auth]).where(Database.auth.c.user_id == uid
+			                                                           and Database.auth.c.refresh_token == refresh_token))
 			if len([dict(r) for r in results]) == 0:
 				log.info("Failed to validate UID + refresh token for UID {}".format(uid))
 				return response("false", "Failed to validate UID+refresh token", True), 403
@@ -126,18 +129,22 @@ class Refresh(Resource):
 			access_token = secrets.token_hex(1024)
 			expiration = datetime.now() + timedelta(seconds=Config.config["auth"]["token-lifetime"])
 
-			engine.execute(sql.update(Database.auth).values(token_expiry=expiration.timestamp() * 1000, access_token=access_token)\
-			               .where(Database.auth.c.refresh_token == refresh_token and Database.auth.c.user_id == uid))
+			engine.execute(sql.update(Database.auth).values(token_expiry=expiration.timestamp() * 1000,
+			                                         access_token=access_token)\
+				.where(and_(Database.auth.c.refresh_token == refresh_token.decode("utf-8"), Database.auth.c.user_id == uid)))
 
 		return {
-			"accessToken": access_token,
-			"expiresIn": Config.config["auth"]["token-lifetime"] * 1000,
-			"expiresAt": int(expiration.timestamp() * 1000)
-		}, 200
+			       "accessToken": access_token,
+			       "expiresIn": Config.config["auth"]["token-lifetime"] * 1000,
+			       "expiresAt": int(expiration.timestamp() * 1000)
+		       }, 200
 
 
 class Unenroll(Resource):
 	@staticmethod
-	def get():
-		"""Deregister (delete login)"""
-		return "Placeholder"
+	@authenticate
+	def get(user):
+		"""Remove access+refresh token from auth database"""
+		with Database() as engine:
+			engine.execute(sql.delete(Database.auth).where(Database.auth.c.user_id == user))
+		return {"success", True}, 200
