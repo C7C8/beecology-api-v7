7b39b6cfbc25519ad80b1c5289ea1c1c6f5ad500
Sourec <tothemyers@gmail.com>
2020-02-05 17:48:01 -0500

    Implement contextmanager for sessions to clean up db code

--- beecology_api/beecology_api/db/__init__.py
+++ beecology_api/beecology_api/db/__init__.py
@@ -1,3 +1,4 @@
+from contextlib import contextmanager
 from logging import getLogger
 
 from sqlalchemy import create_engine
@@ -10,6 +11,7 @@ from .models import *
 Session = None
 
 def init_database():
+	"""Initialize the database"""
 	global Session
 	getLogger().debug("Starting SQL engine for main beecology database")
 	db_config = config.config["database"]
@@ -17,3 +19,12 @@ def init_database():
 	engine = create_engine(cxn_str, pool_size=db_config["pool_size"]) if "sqlite" not in cxn_str else create_engine(cxn_str)
 	BaseTable.metadata.create_all(engine)
 	Session = sessionmaker(bind=engine)
+
+
+@contextmanager
+def db_session():
+	"""Simple database session context manager, for with/as functionality"""
+	global Session
+	session = Session()
+	yield session
+	session.close()
--- beecology_api/beecology_api/endpoints/record.py
+++ beecology_api/beecology_api/endpoints/record.py
@@ -22,17 +22,16 @@ class Record(Resource):
 	def post(self):
 		"""Log a new record. ID and username are ignored and filled in by the server."""
 		# TODO require auth
-		session = db.Session()
-		try:
-			record = bee_record_schema.load(api.payload, session=session)
-		except ValueError:
-			log.error("Flask failed to validate input to Marshmallow's standards: {}".format(api.payload))
-			abort(400, "Invalid input")
+		with db.db_session() as session:
+			try:
+				record = bee_record_schema.load(api.payload, session=session)
+			except ValueError:
+				log.error("Flask failed to validate input to Marshmallow's standards: {}".format(api.payload))
+				abort(400, "Invalid input")
 
-		record.id = uuid4()
-		session.add(record)
-		session.commit()
-		session.close()
+			record.id = uuid4()
+			session.add(record)
+			session.commit()
 
 		return {"message": "Bee record logged"}, 201
 
@@ -41,13 +40,12 @@ class Record(Resource):
 	@api.response(404, "Record not found")
 	def get(self, id: UUID):
 		"""Get one bee record by ID"""
-		session = db.Session()
-		records = session.query(BeeRecord).filter(BeeRecord.id == id).first()
-		if records is None:
-			abort(400, "ID {} not found".format(id))
-		ret = bee_record_schema.dump(records)
-		session.close()
-		return ret
+		with db.db_session() as session:
+			records = session.query(BeeRecord).filter(BeeRecord.id == id).first()
+			if records is None:
+				abort(400, "ID {} not found".format(id))
+			ret = bee_record_schema.dump(records)
+			return ret
 
 	@api.expect(bee_record)
 	@api.response(204, "Bee record updated")
@@ -56,24 +54,21 @@ class Record(Resource):
 	def put(self, id: UUID):
 		"""Update a bee record. Changes to the ID are ignored."""
 		# Delete id+user-id if they exist in the payload
-		session = db.Session()
-		record = session.query(BeeRecord).filter(BeeRecord.id == id).first()
-		if record is None:
-			session.close()
-			abort(404)
+		with db.db_session() as session:
+			record = session.query(BeeRecord).filter(BeeRecord.id == id).first()
+			if record is None:
+				abort(404)
 
-		if "id" in api.payload:
-			api.payload["id"] = str(id)
+			if "id" in api.payload:
+				api.payload["id"] = str(id)
 
-		try:
-			record = bee_record_schema.load(api.payload, session=session)
-		except (ValueError, ValidationError):
-			session.close()
-			abort(400, "Unknown field or data type")
+			try:
+				record = bee_record_schema.load(api.payload, session=session)
+			except (ValueError, ValidationError):
+				abort(400, "Unknown field or data type")
 
-		session.commit()
-		session.close()
-		return "", 204
+			session.commit()
+			return "", 204
 
 	def delete(self):
 		"""Delete a bee record"""
@@ -87,37 +82,35 @@ class Records(Resource):
 	def get(self):
 		"""Get a list of records, filtered by any means."""
 		# TODO Make sure current user is the specified user
-		args = request.args
-		session = db.Session()
-		query = session.query(BeeRecord)
+		with db.db_session() as session:
+			args = request.args
+			query = session.query(BeeRecord)
 
-		# Simple equality filtering
-		for attr in ["user", "species", "flower-species", "abdomen", "thorax", "city", "gender", "behavior"]:
-			if attr in args:
-				query = query.filter(BeeRecord.__dict__[attr] == args[attr])
+			# Simple equality filtering
+			for attr in ["user", "species", "flower-species", "abdomen", "thorax", "city", "gender", "behavior"]:
+				if attr in args:
+					query = query.filter(BeeRecord.__dict__[attr] == args[attr])
 
-		# Range or spatial -based filtering
-		if "max-elevation" in args:
-			query = query.filter(BeeRecord.elevation <= args["max-elevation"])
-		if "min-elevation" in args:
-			query = query.filter(BeeRecord.elevation >= args["min-elevation"])
-		if "time-start" in args:
-			query = query.filter(BeeRecord.time >= args["time-start"])
-		if "time-end" in args:
-			query = query.filter(BeeRecord.time <= args["time-end"])
-		if "bounding-box" in args:
-			# Bounding box is passed in as a list of comma-separated floats: minLat, minLong, maxLat, maxLong
-			try:
-				coords = [float(coord) for coord in args["bounding-box"].split(",")]
-				if len(coords) != 4 or coords[0] >= coords[2] or coords[1] >= coords[3]:
-					raise ValueError
-			except ValueError:
-				session.close()
-				abort(400, "Invalid bounding box filter parameters")
+			# Range or spatial -based filtering
+			if "max-elevation" in args:
+				query = query.filter(BeeRecord.elevation <= args["max-elevation"])
+			if "min-elevation" in args:
+				query = query.filter(BeeRecord.elevation >= args["min-elevation"])
+			if "time-start" in args:
+				query = query.filter(BeeRecord.time >= args["time-start"])
+			if "time-end" in args:
+				query = query.filter(BeeRecord.time <= args["time-end"])
+			if "bounding-box" in args:
+				# Bounding box is passed in as a list of comma-separated floats: minLat, minLong, maxLat, maxLong
+				try:
+					coords = [float(coord) for coord in args["bounding-box"].split(",")]
+					if len(coords) != 4 or coords[0] >= coords[2] or coords[1] >= coords[3]:
+						raise ValueError
+				except ValueError:
+					abort(400, "Invalid bounding box filter parameters")
 
-			query = query.filter(func.ST_Within(BeeRecord.loc_info, func.ST_GeomFromText(
-				"POLYGON(({0} {1}, {2} {1}, {2} {3}, {0} {3}, {0} {1}))".format(*coords))))
+				query = query.filter(func.ST_Within(BeeRecord.loc_info, func.ST_GeomFromText(
+					"POLYGON(({0} {1}, {2} {1}, {2} {3}, {0} {3}, {0} {1}))".format(*coords))))
 
-		ret = [bee_record_schema.dump(record) for record in query.all()], 200
-		session.close()
-		return ret
+			ret = [bee_record_schema.dump(record) for record in query.all()], 200
+			return ret
