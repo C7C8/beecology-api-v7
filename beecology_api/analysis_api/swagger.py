4b63cc60a1a867a216d04cfa1dfbb59d1afe5f91
Sourec <tothemyers@gmail.com>
2020-03-17 11:18:11 -0400

    Fix relative frequencies asking for times as strings

--- beecology_api/analysis_api/swagger.py
+++ beecology_api/analysis_api/swagger.py
@@ -23,7 +23,7 @@ relative_frequencies_result = api.model("Relative frequencies analysis results",
 
 relative_frequencies_parser = bee_record_filter_parser.copy()
 relative_frequencies_parser.add_argument("x-var", type=str, help="X variable", required=True)
-relative_frequencies_parser.add_argument("x-bin-cutoffs", type=str, help="List of X variable cutoffs as timestamps to bin the X axis by (at least 3)", action="split", required=True)
+relative_frequencies_parser.add_argument("x-bin-cutoffs", type=float, help="List of X variable cutoffs as timestamps to bin the X axis by (at least 3)", action="split", required=True)
 relative_frequencies_parser.add_argument("y-var", type=str, help="Y variable", required=True)
 relative_frequencies_parser.add_argument("norm-mode", type=str, help="Normalization mode", choices=["population", "bin"], required=False)
 
